2a6ea6b9846bec0629e1752ca85a3cad7ab24aa3
El-Austa Rami <relausta@ozon.ru>
2021-03-07 21:56:32 +0300

    fixes;

--- scripts/main.js
+++ scripts/main.js
@@ -9,9 +9,11 @@ new Splide('#image-slider', {
 new Splide('#image-slider-2', {
   type: 'loop',
   perPage: 2,
+  perMove: 1,
   classes: {
     arrow : 'splide__arrow my-class-arrow',
+    prev  : 'splide__arrow--prev my-class-arrow-prev',
+		next  : 'splide__arrow--next my-class-arrow-next',
   },
-  pagination: false,
-  gap: '-50px'
+  pagination: false
 }).mount();
